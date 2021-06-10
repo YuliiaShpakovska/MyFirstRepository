3f7615dd1e881c04ea78e44998b855b367e666ae
YuliiaShpakovska <red1878angel@gmail.com>
2021-06-10 14:45:39 -0400

    added to another branch

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -2,6 +2,10 @@ package logic;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello there");
+        int a = 5;
+        int b = 4;
+        if (a + b >= 9) {
+            System.out.println("Hello there");
+        }
     }
 }
