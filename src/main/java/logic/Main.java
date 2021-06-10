ba760558025a4fce4c06198a24c3188d2af44b03
YuliiaShpakovska <red1878angel@gmail.com>
2021-06-10 15:11:36 -0400

    changes to main

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -3,5 +3,7 @@ package logic;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello there");
+        System.out.println("What the hell");
+        System.out.println("and so on and on and on");
     }
 }
